4bae47c1e7d2c9cb9d2d791a4db234410ae05dd8
edamame-creator <edamame@3eansda.com>
2025-09-22 10:53:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const axios = require('axios');
-const cors = require('cors');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
